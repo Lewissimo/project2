c31ac4bae1a340aae87b17ee210d04b4a5c672f9
Kamil Lewiński <kml.lewinski@gmail.com>
2022-12-18 04:49:23 +0100

    sdfads

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.onload = ()=>{
     document.querySelector('body').onclick = (e)=>{
         e.target.style.backgroundColor = "white";
         e.target.style.color = "black";
-        
+        asfdfsfdfads
     }
 }
