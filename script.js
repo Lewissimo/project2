3bdb719b98dea7adc963d5018083259fe8deaa17
Kamil Lewiński <kml.lewinski@gmail.com>
2022-12-18 03:44:22 +0100

    fajno

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = ()=>{
     document.querySelector('body').onclick = (e)=>{
         e.target.style.backgroundColor = "white";
+        e.target.style.color = "black";
+        
     }
 }
